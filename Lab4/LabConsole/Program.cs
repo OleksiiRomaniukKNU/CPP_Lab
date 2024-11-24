aa4d50ea9f237317bf8f781e9d6379fee497d844
OleksiiRomaniukKNU <oleksiiromaniuk@knu.ua>
2024-11-24 19:21:34 +0200

    Lab4

---
+++ Lab4/LabConsole/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using LabLibrary;
+using CommandLine;
+
+namespace MyApp.Console
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('v', "version", HelpText = "Displays version information.")]
+            public bool Version { get; set; }
+
+            [Option("run", HelpText = "Run a specific lab task.")]
+            public string Lab { get; set; }
+
+            [Option('i', "input", HelpText = "Input file.")]
+            public string InputFile { get; set; }
+
+            [Option('o', "output", HelpText = "Output file.")]
+            public string OutputFile { get; set; }
+
+            [Option("set-path", HelpText = "Set the path for input/output files.")]
+            public string SetPath { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(options =>
+                {
+                    // Вивести інформацію про версію
+                    if (options.Version)
+                    {
+                        Console.WriteLine("MyApp Version 1.0.0");
+                        Console.WriteLine("Author: Firstname Lastname");
+                    }
+
+                    // Обробка команди 'run'
+                    else if (!string.IsNullOrEmpty(options.Lab))
+                    {
+                        string inputFile = options.InputFile ?? GetInputFilePath(options.SetPath);
+                        string outputFile = options.OutputFile ?? "output.txt";
+
+                        if (string.IsNullOrEmpty(inputFile))
+                        {
+                            Console.WriteLine("Error: Input file is required.");
+                            return;
+                        }
+
+                        switch (options.Lab.ToLower())
+                        {
+                            case "lab1":
+                                LabTasks.RunLab1(inputFile, outputFile);
+                                break;
+                            case "lab2":
+                                LabTasks.RunLab2(inputFile, outputFile);
+                                break;
+                            case "lab3":
+                                LabTasks.RunLab3(inputFile, outputFile);
+                                break;
+                            default:
+                                Console.WriteLine("Invalid lab number. Use 'lab1', 'lab2', or 'lab3'.");
+                                break;
+                        }
+                    }
+                    // Обробка команди set-path
+                    else if (!string.IsNullOrEmpty(options.SetPath))
+                    {
+                        Environment.SetEnvironmentVariable("LAB_PATH", options.SetPath);
+                        Console.WriteLine($"LAB_PATH set to: {options.SetPath}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid command. Use 'version', 'run <lab>', or 'set-path <path>'.");
+                    }
+                });
+        }
+
+        private static string GetInputFilePath(string customPath)
+        {
+            string filePath = customPath ?? Environment.GetEnvironmentVariable("LAB_PATH");
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "input.txt");
+            }
+
+            return filePath;
+        }
+    }
+}
---
+++ Lab4/LabLibrary/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace LabLibrary
+{
+    public class LabTasks
+    {
+        public static void RunLab1(string inputFile, string outputFile)
+        {
+            // Реалізація для лабораторної роботи 1
+            Console.WriteLine("Running Lab1...");
+            // Тут буде код для виконання практичної роботи 1
+        }
+
+        public static void RunLab2(string inputFile, string outputFile)
+        {
+            // Реалізація для лабораторної роботи 2
+            Console.WriteLine("Running Lab2...");
+            // Тут буде код для виконання практичної роботи 2
+        }
+
+        public static void RunLab3(string inputFile, string outputFile)
+        {
+            // Реалізація для лабораторної роботи 3
+            Console.WriteLine("Running Lab3...");
+            // Тут буде код для виконання практичної роботи 3
+        }
+    }
+}
